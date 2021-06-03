5483bc39b971e12ec5f5abc15e50aa7484cf6cdf
QAQLiuCong <794418142@qq.com>
2021-06-03 09:58:43 +0800

    调整

--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -1,22 +1,28 @@
 <template>
-  <img v-show="show" src="../../assets/logo.png" alt="">
+  <img v-show="show" src="../../assets/logo.png" alt="" />
   <el-button v-on:click="show = !show">点击显示/隐藏</el-button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import {
+  defineComponent,
+  ref,
+  getCurrentInstance,
+} from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  setup () {
-    const show = ref<boolean>(true)
+  name: "Home",
+  setup() {
+    const { appContext }: any = getCurrentInstance();
+    const $app = appContext.config.globalProperties;
+    console.log($app);
+    const show = ref<boolean>(true);
     return {
       show
-    }
+    };
   }
-})
+});
 </script>
 
 <style lang="scss" scoped>
-
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -7,55 +7,61 @@ const resolve = (dir: string) => path.join(__dirname, dir)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		styleImport({
-      libs: [{
-        libraryName: 'element-plus',
-        esModule: true,
-        ensureStyleFile: true,
-        resolveStyle: (name) => {
-          name = name.slice(3)
-          return `element-plus/packages/theme-chalk/src/${name}.scss`;
-        },
-        resolveComponent: (name) => {
-          return `element-plus/lib/${name}`;
-        },
-      }]
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          ensureStyleFile: true,
+          resolveStyle: (name) => {
+            name = name.slice(3)
+            return `element-plus/packages/theme-chalk/src/${name}.scss`
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`
+          }
+        }
+      ]
     }),
-		legacy({
+    legacy({
       targets: ['defaults', 'not IE 11']
     })
-	],
-	css: {
+  ],
+  css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `$injectedColor: orange;`
+        // 引入 var.scss 这样就可以在全局中使用 var.scss中预定义的变量了
+        // 给导入的路径最后加上 ;
+        additionalData: '@import "./src/assets/scss/var.scss";'
       }
     }
   },
   resolve: {
-		alias: {
-			'@': resolve('src')
-		}
+    alias: {
+      '@': resolve('src')
+    }
   },
   server: {
-		//服务器主机名
-		host: '',
-		//端口号
-		port: 8888,
-		//设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
-		strictPort: false,
-		//服务器启动时自动在浏览器中打开应用程序,当此值为字符串时，会被用作 URL 的路径名
+    //服务器主机名
+    host: '',
+    //端口号
+    port: 8888,
+    //设为 true 时若端口已被占用则会直接退出，而不是尝试下一个可用端口
+    strictPort: false,
+    //服务器启动时自动在浏览器中打开应用程序,当此值为字符串时，会被用作 URL 的路径名
 		open: false,
-		//自定义代理规则
-		proxy: {
-			// 选项写法
-			'/api': {
-				// target: 'http://jsonplaceholder.typicode.com',
-				changeOrigin: true,
-				rewrite: path => path.replace(/^\/api/, '')
-			}
-		}
-	}
+		// 为开发服务器配置 CORS。默认启用并允许任何源，传递一个 选项对象 来调整行为或设为 false 表示禁用。
+		cors: true,
+    //自定义代理规则
+    proxy: {
+      // 选项写法
+      '/api': {
+        target: '',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
